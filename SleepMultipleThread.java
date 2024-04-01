e55d46923db23d878780d63ec37effacc7827156
vidhijain2024 <165646564+vidhijain2024@users.noreply.github.com>
2024-04-01 14:16:42 +0530

    Add files via upload

---
+++ SleepMultipleThread.java
@@ -0,0 +1,23 @@
+package sleepmultiplethread;
+import java.io.*;
+import java.lang.Thread;
+
+public class SleepMultipleThread {
+    public static void main(String[] args) {
+     
+    try
+    {
+        for(int i=0;i<5;i++)
+    {
+    Thread.sleep(1000);
+    System.out.println(i);
+    }
+   }   
+    catch(Exception e)
+    {
+        System.out.println(e);
+    }
+    }
+    }
+    
+
